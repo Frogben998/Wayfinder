fbebb75afe91568fc16e7f31d01341f056bcbdf4
RushB998 <b.rush@northeastern.edu>
2025-02-10 12:04:42 -0500

    making project, copy pasting gitignore and gitattributes file from other project to make this work

---
+++ HapitcStudy/Assets/Scripts/CollisionHaptic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CollisionHaptic : MonoBehaviour
+{
+
+    XRBaseController thisController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
