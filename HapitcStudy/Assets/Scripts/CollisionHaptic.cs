014c47ac84b7fbebd1537d3d039fc3cb195e1b9e
RushB998 <b.rush@northeastern.edu>
2025-02-13 17:48:58 -0500

    adding Oculus haptic SDK, and Haptic studio files etc

--- HapitcStudy/Assets/Scripts/CollisionHaptic.cs
+++ HapitcStudy/Assets/Scripts/CollisionHaptic.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+//using UnityEngine.XRModule;
 
 public class CollisionHaptic : MonoBehaviour
 {
-
+    [System.Obsolete]
     XRBaseController thisController;
+    public Collider controllerCollider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [System.Obsolete]
     void Start()
     {
-        
+        thisController = GetComponent<XRBaseController>();
     }
 
     // Update is called once per frame
@@ -19,4 +22,23 @@ public class CollisionHaptic : MonoBehaviour
     {
         
     }
+
+    [System.Obsolete]
+    public void SendHapticFeedback(BaseInteractionEventArgs args)
+    {
+        args.interactorObject.transform.GetComponent<XRBaseController>().SendHapticImpulse(1f,2f);
+        //thisController.SendHapticImpulse(1f, 1.5f);
+    }
+
+    [System.Obsolete]
+    public void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "Sandpaper")
+        {
+            //SendHapticFeedback();
+            thisController.SendHapticImpulse(1f, 1f);
+        }
+    }
+
+
 }
