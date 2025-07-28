5b4520d81b67adb7399043f126defbe531979484
Frogben998 <jdsbrush@live.com>
2025-07-28 17:30:48 -0400

    Moving Player Spawn, recreating canvas module in wake of screw up

--- HapitcStudy/Assets/StayInPlace.cs
+++ HapitcStudy/Assets/StayInPlace.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class StayInPlace : MonoBehaviour
 {
+    private Vector3 initialPlace;
+    private Quaternion initialRotation;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        initialPlace = gameObject.transform.position;
+        initialRotation = gameObject.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = initialPlace;
+        gameObject.transform.rotation = initialRotation;
     }
 }
