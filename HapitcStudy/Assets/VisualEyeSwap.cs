39b10dbccee7e410e001443b902732969856f587
Frogben998 <jdsbrush@live.com>
2025-08-01 15:33:53 -0400

    Replacing tag based system with setactive system

--- HapitcStudy/Assets/VisualEyeSwap.cs
+++ HapitcStudy/Assets/VisualEyeSwap.cs
@@ -29,11 +29,14 @@ public class VisualEyeSwap : MonoBehaviour
 
     // Unsure if this is the script to have the function in, but at the very least I'll have it built out.
     // ok what this thing is gonna try to do is swap the ActiveVisualEye tag from gameobject to gameobject so that the preview mechanic can work. 
+    //EDIT: Tag based system is a bust.
     public void VisualEyeSwapper()
     {
         if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.LTouch)){
             currentVisualEyeIndex++;
         }
+
+
         Debug.Log("The current Visual Eye Index is: " + currentVisualEyeIndex);
         switch (currentVisualEyeIndex)
         {
@@ -42,20 +45,50 @@ public class VisualEyeSwap : MonoBehaviour
                 break;
             case 1:
                 visualEyeColor.tag = "ActiveVisualEye";
-                visualEyeColor.SetActive(true);
+                //visualEyeColor.SetActive(true);
+                if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyeColor.SetActive(true);
+                }
+                else if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyeColor.SetActive(false);
+                }
                 break;
             case 2:
-                visualEyeColor.tag = "Untagged";
                 visualEyeColor.SetActive(false);
-                visualEyeHemianopia.SetActive(true);
+                if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyeHemianopia.SetActive(true);
+                }
+                else if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyeHemianopia.SetActive(false);
+                }
                 break;
             case 3:
                 visualEyeHemianopia.SetActive(false);
-                visualEyePeripheralLoss.SetActive(true);
+                if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyePeripheralLoss.SetActive(true);
+                }
+                else if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyePeripheralLoss.SetActive(false);
+                }
+                //visualEyePeripheralLoss.SetActive(true);
                 break;
             case 4:
                 visualEyePeripheralLoss.SetActive(false);
-                visualEyeLowContrast.SetActive(true);
+                if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyeLowContrast.SetActive(true);
+                }
+                else if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+                {
+                    visualEyeLowContrast.SetActive(false);
+                }
+                //visualEyeLowContrast.SetActive(true);
                 break;
             case 5:
                 visualEyeLowContrast.SetActive(false);
