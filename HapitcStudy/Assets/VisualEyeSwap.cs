d1f1d53d6caf482c223f70336871e9e88af93c80
Frogben998 <jdsbrush@live.com>
2025-07-31 21:48:23 -0400

    Creating visualeyeswap script

---
+++ HapitcStudy/Assets/VisualEyeSwap.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+
+public class VisualEyeSwap : MonoBehaviour
+{
+    public GameObject visualEyeColor;
+    public GameObject visualEyeHemianopia;
+    public GameObject visualEyePeripheralLoss;
+    public GameObject totalBlindnessLight;
+    public GameObject visualEyeLowContrast;
+
+    // key line of code to know:  if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.LTouch)
+    //B and Y buttons.
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+}
