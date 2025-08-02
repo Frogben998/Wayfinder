993ff9c9b7c4c00c8289865a111fec96c8016bdf
Frogben998 <jdsbrush@live.com>
2025-08-02 16:51:56 -0400

    description and controls created

---
+++ HapitcStudy/Assets/ChosenVisualTextDisplay.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChosenVisualTextDisplay : MonoBehaviour
+{
+    [SerializeField] public TextMeshPro chosenVisualEye;
+    [SerializeField] public TextMeshPro chosenVisualEyeDescription;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
